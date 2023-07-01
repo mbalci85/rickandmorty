f1f3f30db6d79f0a42058dd42c10b993ee9d6ee2
mustafa balci <mbalci85@gmail.com>
2023-07-01 12:19:27 +0100

    filter func updated

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,9 @@
 const BASE_URL = 'https://rickandmortyapi.com/api';
 const imgsContainer = document.querySelector('#all-imgs');
 const pageNumInput = document.querySelector('#page-count-input');
+const pageNumHeader = document.querySelector('#page-num');
+const searchByName = document.querySelector('#search');
+let pageCount;
 
 const getAllChars = async (pageNum) => {
 	try {
@@ -8,9 +11,11 @@ const getAllChars = async (pageNum) => {
 		const data = await response.json();
 
 		const characters = data.results;
-		const pageCount = data.info.pages;
+		pageCount = data.info.pages;
 
 		pageNumInput.placeholder = `Up to ${pageCount}`;
+		pageNumHeader.innerHTML = `Page ${pageNum}`;
+		searchByName.value = '';
 
 		imgsContainer.innerHTML = characters
 			.map((char) => {
@@ -34,6 +39,33 @@ pageForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 	getAllChars(pageNumInput.value);
 	pageNumInput.value = '';
+});
+
+searchByName.addEventListener('input', async (e) => {
+	try {
+		const response = await fetch(`${BASE_URL}/character/?name=${e.target.value}`);
+		const data = await response.json();
+		const filteredChars = data.results;
+		pageCount = data.info.pages;
+		imgsContainer.innerHTML = '';
+		pageNumInput.placeholder = `Up to ${pageCount}`;
+
+		imgsContainer.innerHTML = filteredChars
+			.map((char) => {
+				return `<div class='img-container'>
+			<img src='${char.image}'>
+			<div class='img-card-info'>
+				<p>${char.name}</p>
+				<a href="../pages/detail.html?id=${char.id}">See Details</a>
+			</div>
+			</div>`;
+			})
+			.join('');
+	} catch (error) {
+		console.log(error);
+		imgsContainer.innerHTML = '';
+		pageNumInput.placeholder = '';
+	}
 });
 
 getAllChars(1);
