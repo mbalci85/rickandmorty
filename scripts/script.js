247f2a877474b4b1825d5f825776b935989bdb60
mustafa balci <mbalci85@gmail.com>
2023-07-02 13:43:48 +0100

    next page func added

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,16 @@
 const BASE_URL = 'https://rickandmortyapi.com/api';
-const imgsContainer = document.querySelector('#all-imgs');
-const pageNumInput = document.querySelector('#page-count-input');
-const pageNumHeader = document.querySelector('#page-num');
-const searchByName = document.querySelector('#search');
+let imgsContainer = document.querySelector('#all-imgs');
+let pageNumInput = document.querySelector('#page-count-input');
+let pageNumHeader = document.querySelector('#page-num');
+let searchByName = document.querySelector('#search');
+let prevBtn = document.querySelector('#prev-btn');
+let nextBtn = document.querySelector('#next-btn');
 let pageCount;
+let nextLink;
+let prevLink;
+let currentPage = 1;
 
-const getAllChars = async (pageNum) => {
+const getAllChars = async (pageNum = 1) => {
 	try {
 		const response = await fetch(`${BASE_URL}/character/?page=${pageNum}`);
 		const data = await response.json();
@@ -13,26 +18,21 @@ const getAllChars = async (pageNum) => {
 		const characters = data.results;
 		pageCount = data.info.pages;
 
+		nextLink = data.info.next;
+		prevLink = data.info.prev;
+
 		pageNumInput.placeholder = `Up to ${pageCount}`;
 		pageNumHeader.innerHTML = `Page ${pageNum}`;
+		currentPage = pageNum;
 		searchByName.value = '';
 
-		imgsContainer.innerHTML = characters
-			.map((char) => {
-				return `<div class='img-container'>
-			<img src='${char.image}'>
-			<div class='img-card-info'>
-				<p>${char.name}</p>
-				<a href="../pages/detail.html?id=${char.id}">See Details</a>
-			</div>
-			</div>`;
-			})
-			.join('');
+		updateImgContainer(characters);
 	} catch (error) {
 		console.log(error);
 	}
 };
 
+//Go to Page Function
 const pageForm = document.querySelector('#go-to-page');
 
 pageForm.addEventListener('submit', (e) => {
@@ -41,31 +41,77 @@ pageForm.addEventListener('submit', (e) => {
 	pageNumInput.value = '';
 });
 
+//Search by Name Function
 searchByName.addEventListener('input', async (e) => {
 	try {
 		const response = await fetch(`${BASE_URL}/character/?name=${e.target.value}`);
 		const data = await response.json();
 		const filteredChars = data.results;
 		pageCount = data.info.pages;
-		imgsContainer.innerHTML = '';
 		pageNumInput.placeholder = `Up to ${pageCount}`;
+		pageNumHeader.innerHTML = 'Page 1';
 
-		imgsContainer.innerHTML = filteredChars
-			.map((char) => {
-				return `<div class='img-container'>
-			<img src='${char.image}'>
-			<div class='img-card-info'>
-				<p>${char.name}</p>
-				<a href="../pages/detail.html?id=${char.id}">See Details</a>
-			</div>
-			</div>`;
-			})
-			.join('');
+		updateImgContainer(filteredChars);
 	} catch (error) {
 		console.log(error);
 		imgsContainer.innerHTML = '';
 		pageNumInput.placeholder = '';
+		pageNumHeader.innerHTML = '';
 	}
 });
 
-getAllChars(1);
+getAllChars();
+
+//Previous Page Function
+prevBtn.addEventListener('click', async (e) => {
+	e.preventDefault();
+	if (prevLink != null) {
+		try {
+			const response = await fetch(prevLink);
+			const data = await response.json();
+
+			const characters = data.results;
+			updateImgContainer(characters);
+			prevLink = data.info.prev;
+			nextLink = data.info.next;
+			currentPage--;
+			pageNumHeader.innerHTML = `Page ${currentPage}`;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+});
+
+//Next Button Function
+nextBtn.addEventListener('click', async (e) => {
+	e.preventDefault();
+	if (nextLink != null) {
+		try {
+			const response = await fetch(nextLink);
+			const data = await response.json();
+			const nextCharacters = data.results;
+			updateImgContainer(nextCharacters);
+			nextLink = data.info.next;
+			prevLink = data.info.prev;
+
+			currentPage++;
+			pageNumHeader.innerHTML = `Page ${currentPage}`;
+		} catch (error) {
+			console.log(error);
+		}
+	}
+});
+
+const updateImgContainer = (arr) => {
+	imgsContainer.innerHTML = arr
+		.map((char) => {
+			return `<div class='img-container'>
+	<img src='${char.image}'>
+	<div class='img-card-info'>
+		<p>${char.name}</p>
+		<a href="../pages/detail.html?id=${char.id}">See Details</a>
+	</div>
+	</div>`;
+		})
+		.join('');
+};
