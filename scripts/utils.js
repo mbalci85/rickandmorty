7e56e814f0b6980c0c90d343f4998c6fb81a7352
mustafa balci <mbalci85@gmail.com>
2023-07-04 13:21:28 +0100

    add to fav

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,5 +1,7 @@
 export let imgsContainer = document.querySelector('#all-imgs');
-
+const addToFav = () => {
+	console.log('hello');
+};
 //updating list of chars when next, prev, search, showall chars
 export const updateImgContainer = (arr) => {
 	imgsContainer.innerHTML = arr
@@ -8,6 +10,8 @@ export const updateImgContainer = (arr) => {
 	<img src='${char.image}'>
 	<div class='img-card-info'>
 		<p>${char.name}</p>
+		<i id='add-fav' onclick="addToFav()" class="fas fa-heart"></i>
+
 		<a href="../pages/detail.html?id=${char.id}">See Details</a>
 	</div>
 	</div>`;
